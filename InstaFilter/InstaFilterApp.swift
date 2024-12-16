67e65f9cdcb201bd8d42936b760117c2ccea3bd8
Constantin Lisnic <constantin@lisnic.dev>
2024-12-16 13:57:36 +0000

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Constantin Lisnic on 16/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstaFilterTests/InstaFilterTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterTests.swift
+//  InstaFilterTests
+//
+//  Created by Constantin Lisnic on 16/12/2024.
+//
+
+import Testing
+@testable import InstaFilter
+
+struct InstaFilterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
